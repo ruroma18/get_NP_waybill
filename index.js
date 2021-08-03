334b0ca891edbfe136544bd6cf8730b4142edd7c
ruroma18 <57109615+ruroma18@users.noreply.github.com>
2021-08-03 14:27:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let myHeaders = new Headers();
 myHeaders.append("Content-Type", "text/plain");
 myHeaders.append("Cookie", "PHPSESSID=58235f830633492d9c13b243c718d2f8; YIICSRFTOKEN=9764a6debc7d5fad5df6c76c86f245603b9a1da4s%3A88%3A%22VWJTMkRENjBZa1FDUkZvT292eVBLeXlTblNZSk5CbmaSKVwQwb8QOWL7OMp3e92LV6KdoZFaQ8lQOsK89L9Z4A%3D%3D%22%3B");
 
-let raw = "{\r\n\"apiKey\": \"41de3ecc2b398986e37a03631f257f8d\",\r\n\"modelName\": \"InternetDocument\",\r\n\"calledMethod\": \"getDocumentList\",\r\n\"methodProperties\": {\r\n\"DateTime\": \"12.12.2020\",\r\n\"Page\": \"1\",\r\n\"GetFullList\": \"0\"\r\n}\r\n}";
+let raw = "{\r\n\"apiKey\": \"d01aa3752730451f5b0b3d3f0dcaddeb\",\r\n\"modelName\": \"InternetDocument\",\r\n\"calledMethod\": \"getDocumentList\",\r\n\"methodProperties\": {\r\n\"DateTime\": \"12.12.2020\",\r\n\"Page\": \"1\",\r\n\"GetFullList\": \"0\"\r\n}\r\n}";
 
 let requestOptions = {
     method: 'POST',
