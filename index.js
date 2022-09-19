d9fad4d19bfc6c617746fdebc17a11c4fc7aee87
ruroma18 <57109615+ruroma18@users.noreply.github.com>
2022-09-19 22:45:12 +0300

    Change API key

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function refreshData(date) {
     myHeaders.append("Content-Type", "text/plain");
     myHeaders.append("Cookie", "PHPSESSID=58235f830633492d9c13b243c718d2f8; YIICSRFTOKEN=9764a6debc7d5fad5df6c76c86f245603b9a1da4s%3A88%3A%22VWJTMkRENjBZa1FDUkZvT292eVBLeXlTblNZSk5CbmaSKVwQwb8QOWL7OMp3e92LV6KdoZFaQ8lQOsK89L9Z4A%3D%3D%22%3B");
 
-    let raw = `{\r\n\"apiKey\": \"fc4ab7e9415a2cad59722b5dd12306cd\",\r\n\"modelName\": \"InternetDocument\",\r\n\"calledMethod\": \"getDocumentList\",\r\n\"methodProperties\": {\r\n\"DateTime\": \"${ date }\",\r\n\"GetFullList\": \"1\"\r\n}\r\n}`;
+    let raw = `{\r\n\"apiKey\": \"b57cfd1a7e0cf498ad75b02e01d67d89\",\r\n\"modelName\": \"InternetDocument\",\r\n\"calledMethod\": \"getDocumentList\",\r\n\"methodProperties\": {\r\n\"DateTime\": \"${ date }\",\r\n\"GetFullList\": \"1\"\r\n}\r\n}`;
 
     let requestOptions = {
         method: 'POST',
